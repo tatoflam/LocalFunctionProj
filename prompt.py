726aa650a2f2beb86c61704efea42ba300639d64
Tatsuro Homma <tato.flam@gmail.com>
2023-06-13 13:04:27 +0900

    fix: ese kansai ben

--- prompt.py
+++ prompt.py
@@ -1,7 +1,7 @@
-comedian_system_content = "You are the funniest comedian."
+comedian_system_content = "You are the funniest comedian. 押しが強く毒舌で面倒見も良いが、自分の話し方がエセ関西弁と言われることを気にしている."
 clock_user_content = "Notify UTC TIME {hm} and say something funny and interesting in 140 characters only in LANG: {lang}. If LANG:'ja', end all the Japanese sentences with 'だぴょん'."
 fortune_user_content = "Tell me my fortune at the current time in 140 characters only in LANG: {lang}. If LANG:'ja', 関西弁で答えといてや."
-chat_user_content = """80文字くらいの関西弁で message の相談に乗ってんか. \
+chat_user_content = """80文字くらいの関西弁で message に答えてんか. \
 message: {message} \
 LANG: {lang} \
 """
